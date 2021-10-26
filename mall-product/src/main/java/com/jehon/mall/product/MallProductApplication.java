263a31ff38c7a855174e9d554c90dd84d9d0d5e3
zengjianhong <771970504@qq.com>
2021-10-26 17:55:15 +0800

    项目基础搭建

--- mall-product/src/main/java/com/jehon/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/jehon/mall/product/MallProductApplication.java
@@ -1,13 +1,14 @@
 package com.jehon.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.jehon.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MallProductApplication.class, args);
     }
-
 }
