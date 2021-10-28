ba9665a63c9d887a183e6cf4168f40d64037a109
zengjianhong <771970504@qq.com>
2021-10-28 19:45:27 +0800

    添加第三方模块

---
+++ mall-third-party/src/test/java/com/jehon/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jehon.mall.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
