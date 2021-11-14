7c7e3b16cb7090334eebc147f57d14af403d6c18
zengjianhong <771970504@qq.com>
2021-11-14 10:43:55 +0800

    添加认证服务

---
+++ mall-auth-server/src/test/java/com/jehon/mallauthserver/MallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jehon.mallauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
