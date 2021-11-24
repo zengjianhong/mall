7dc1dae29cb1d7e73a39bfaa2beec4c4917f3121
zengjianhong <771970504@qq.com>
2021-11-24 10:57:19 +0800

    测试单点登录

---
+++ mall-test-sso-server/src/test/java/com/jehon/malltestssoserver/MallTestSsoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jehon.malltestssoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTestSsoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
